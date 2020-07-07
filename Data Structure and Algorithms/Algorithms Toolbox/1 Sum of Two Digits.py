100e26f519dc092980c3eedd4c59451e99995dbc
unknown <deep.chokshi@outlook.com>
2020-07-07 11:16:00 +0530

    Added

---
+++ Data Structure and Algorithms/Algorithms Toolbox/1 Sum of Two Digits.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 17:47:04 2020
+
+@author: Deep Chokshi
+"""
+#sum of two digit
+
+num1, num2 = map(int, input().split()) 
+sum = num1 + num2
+print (sum)
